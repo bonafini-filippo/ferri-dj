59b0ed11ea75210f183a1d92244575ab4862be33
Webcraft <bonafini.filippo@icloud.com>
2025-01-16 19:45:55 +0000

    refactor ScoreElo component for responsive design; adjust icon sizes and layout

--- components/ScoreElo.tsx
+++ components/ScoreElo.tsx
@@ -18,12 +18,15 @@ const ScoreElo = async () => {
         <span className="absolute right-4 top-4 text-2xl">
           <IoIosArrowDropright size={32} />
         </span>
-        <div className="rounded-xl overflow-hidden">
+        <div className="rounded-xl w-[100px] h-[100px] md:w-[150px] md:h-[150px] overflow-hidden">
           <Image src={ChessLogo} width={150} height={150} alt="Chess.com" />
         </div>
-        <div className="text-xl tracking-wider">
+        <div className="md:text-xl tracking-wider">
           <div className="flex  items-center gap-2">
-            <FaStopwatch size={28} />
+            <FaStopwatch
+              size={28}
+              className="w-[18px] h-[18px] md:w-[28px] md:h-[28px] "
+            />
             <span>
               <span className="font-light">Rapid: </span>
               <span className="font-semibold">
@@ -32,7 +35,10 @@ const ScoreElo = async () => {
             </span>
           </div>
           <div className="flex items-center gap-2 my-4">
-            <FaBolt size={28} />
+            <FaBolt
+              size={28}
+              className="w-[18px] h-[18px] md:w-[28px] md:h-[28px] "
+            />
             <span>
               <span className="font-light">Blitz: </span>
               <span className="font-semibold">
@@ -41,7 +47,7 @@ const ScoreElo = async () => {
             </span>
           </div>
           <div className="flex items-center gap-2 ">
-            <div className="flex justify-center items-center w-[28px] h-[28px] ">
+            <div className="flex justify-center items-center  w-[18px] h-[18px] md:w-[28px] md:h-[28px]">
               <Image src={Bullet} width={22} height={22} alt="bullet" />
             </div>
 
