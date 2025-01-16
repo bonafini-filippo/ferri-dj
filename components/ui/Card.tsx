c48d08890b4de60657c6f9e92034fd98fff2d3e5
Webcraft <bonafini.filippo@icloud.com>
2025-01-16 14:27:49 +0000

    test 1

--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -8,18 +8,20 @@ interface CardProps {
 
 const Card = ({ title, description, image }: CardProps) => {
   return (
-    <div className="bg-[#2a2a2a] max-w-[390px] flex shrink-0 justify-center items-center gap-5 overflow-hidden rounded-lg min-h-[150px] min-w-[150px]">
-      <div className="h-[150px] w-[150px]  shrink-0">
+    <div className="bg-[#2a2a2a] max-w-[290px] md:max-w-[390px] flex shrink-0 justify-center items-center gap-5 overflow-hidden rounded-lg md:min-h-[150px] md:min-w-[150px] min-h-[100px] min-w-[100px]">
+      <div className="h-[120px] w-[120px] md:h-[150px] md:w-[150px]  shrink-0">
         <Image
           className="object-cover h-full w-full"
-          width={150}
-          height={150}
+          width={250}
+          height={250}
           src={image}
-          alt="image"
+          alt={title}
         />
       </div>
-      <div className="font-light grow">
-        <h1 className="text-xl mb-1 line-clamp-2 font-semibold">{title}</h1>
+      <div className="font-light grow ">
+        <h1 className="md:text-xl text-lg mb-1 line-clamp-2 font-semibold">
+          {title}
+        </h1>
         <p className="line-clamp-2 text-sm">{description}</p>
       </div>
     </div>
