b47439b862f1d0389061b0ceca959f4763287bfd
Webcraft <bonafini.filippo@icloud.com>
2025-01-27 17:17:14 +0000

    refactor Card component for improved layout and styling; adjust alignment and add fixed aspect ratio for image container

--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -12,19 +12,20 @@ const Card = ({ title, description, image, video_id }: CardProps) => {
   return (
     <Link
       href={'https://www.youtube.com/watch?v=' + video_id}
-      className="bg-[#2a2a2a] max-w-[300px] md:max-w-[390px] flex shrink-0 justify-center items-center gap-4 overflow-hidden rounded-lg md:min-h-[150px] md:min-w-[150px] min-h-[120px] min-w-[120px]"
+      className="bg-[#2a2a2a] max-w-[300px] md:max-w-[390px] flex shrink-0 justify-start items-center gap-4 overflow-hidden rounded-lg md:min-h-[150px] md:min-w-[150px] min-h-[120px] min-w-[120px]"
     >
-      <div className="h-[120px] w-[120px] md:h-[150px] md:w-[150px]  shrink-0">
+      {/* Contenitore immagine con proporzioni fisse */}
+      <div className="flex-shrink-0 relative h-[120px] w-[120px] md:h-[150px] md:w-[150px]">
         <Image
-          className="object-cover h-full w-full"
-          width={250}
-          height={250}
+          className="object-cover rounded-md"
+          fill
           src={image}
           alt={title}
         />
       </div>
-      <div className="font-light grow">
-        <h1 className="md:text-xl text-lg mb-1 mr-10 line-clamp-2 font-semibold">
+      {/* Contenitore testo */}
+      <div className="grow overflow-hidden p-1">
+        <h1 className="md:text-xl text-lg mb-1 mr-2 line-clamp-2 font-semibold">
           {title}
         </h1>
         <p className="line-clamp-2 text-sm">{description}</p>
